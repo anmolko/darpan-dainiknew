7b3b566433733e0ccae5afb742ff2e9dd86b98ff
anmolko <anmol@canosoft.com.np>
2023-01-11 12:16:55 +0545

    view in frontend update

--- resources/views/backend/blog/edit.blade.php
+++ resources/views/backend/blog/edit.blade.php
@@ -72,7 +72,8 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Edit Blog</h4>
+                        <h4 class="mb-sm-0">Edit Blog
+                        </h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
@@ -91,7 +92,14 @@
             <div class="row">
 
                 <div class="col-lg-8">
-
+                    <div class="card-header align-items-center d-flex">
+                            <h4 class="card-title mb-0 flex-grow-1">General</h4>
+                        <div class="flex-shrink-0">
+                            <a  href="{{route('blog.single',$edit->slug)}}" target="_blank" class="btn btn-soft-primary btn-sm"">
+                               <i class=" ri-eye-line align-middle"></i> View in frontend
+                            </a>
+                        </div>
+                    </div>
                         <div class="card">
                             <div class="card-body">
                                 <div class="mb-3">
